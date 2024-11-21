619b9b29721195d45b4669f7a8e3bf7205d309ca
JAYASREE1540 <159135389+JAYASREE1540@users.noreply.github.com>
2024-11-21 12:38:19 +0530

    Update First palindrome string.py

--- First palindrome string.py
+++ First palindrome string.py
@@ -1,10 +1,8 @@
-def first_palindrome(strings):
-    for s in strings:
-        if s == s[::-1]:  # Check if the string is a palindrome
-            return s
-    return "No palindrome found"
-
-# Example usage
 strings = ["apple", "level", "banana", "radar"]
-result = first_palindrome(strings)
-print("First palindrome string:", result)
+
+for s in strings:
+    if s == s[::-1]:  # Check if the string is a palindrome
+        print("First palindrome string:", s)
+        break
+else:
+    print("No palindrome found")
